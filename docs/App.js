bacb664e995baf551459107526311f269cc25bcd
knipferrc <knipferrc@gmail.com>
2017-06-15 13:42:07 -0500

    basic modal

--- docs/App.js
+++ docs/App.js
@@ -1,14 +1,36 @@
-import { Button, Card, Loader, TextArea, TextField } from 'stylized'
+import { Button, Card, Loader, Modal, TextArea, TextField } from 'stylized'
+import React, { Component } from 'react'
 
-import React from 'react'
+export default class extends Component {
+  state = {
+    open: false
+  }
 
-export default () => 
-  <div>
-    <Button>Test</Button>
-    <Card>
-      Some content here
-    </Card>
-    <TextField placeholder="Enter some text" />
-    <TextArea placeholder="Enter some long text" />
-    <Loader />
-  </div>
+  toggle = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.toggle}>Test</Button>
+        <Card>
+          Some content here
+        </Card>
+        <TextField placeholder="Enter some text" />
+        <TextArea placeholder="Enter some long text" />
+        <Loader />
+        {this.state.open && 
+          <Modal>
+            <TextField placeholder="Test" />
+            <TextField placeholder="Test" />
+            <TextField placeholder="Test" />
+            <Button onClick={this.toggle}>Test</Button>
+          </Modal>
+        }
+      </div>
+    )
+  }
+}
---
+++ src/Modal/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const ModalContainer = styled.div`
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.95);
+  color: dimgrey;
+  z-index: 100;
+  width: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Modal = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 3px;
+  margin-left: 10px;
+  margin-right: 10px;
+`
+
+export default ({ children }) =>
+  <ModalContainer>
+    <Modal>
+      {children}
+    </Modal>
+  </ModalContainer>
