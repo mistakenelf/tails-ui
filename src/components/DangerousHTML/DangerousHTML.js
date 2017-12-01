b580f71b23e999b7e53e802e2e220230f9859be5
knipferrc <knipferrc@gmail.com>
2017-12-01 11:18:34 -0500

    sans font

--- src/components/DangerousHTML/DangerousHTML.js
+++ src/components/DangerousHTML/DangerousHTML.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const DangerousHTML = ({ children }) => (
-  <div dangerouslySetInnerHTML={{ __html: children }} />
+  <div className="font-sans" dangerouslySetInnerHTML={{ __html: children }} />
 )
 
 DangerousHTML.propTypes = {
