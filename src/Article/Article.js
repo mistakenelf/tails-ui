b28f564edbbb72bb84fe02434e9d77365bbdbbf2
knipferrc <knipferrc@gmail.com>
2017-12-06 12:59:53 -0600

    allow footer content

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,16 +1,14 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Article = ({ title, author, description, abstract }) => (
+const Article = ({ title, author, description, abstract, footer }) => (
   <div className="w-full sm:w-1/2 md:w-1/3">
     <h1 className="font-sans font-thin mb-4">{title}</h1>
     <p className="text-grey mb-3">{author}</p>
     <h2 className="font-sans font-thin leading-normal mb-4">{description}</h2>
     <p className="text-grey-darkest mb-6 leading-tight">{abstract}</p>
     <div>
-      <a href="#" className="text-black mr-2">
-        READ MORE
-      </a>
+      {footer}
     </div>
   </div>
 )
@@ -19,7 +17,8 @@ Article.propTypes = {
   title: PropTypes.string,
   author: PropTypes.string,
   description: PropTypes.string,
-  abstract: PropTypes.string
+  abstract: PropTypes.string,
+  footer: PropTypes.node
 }
 
 export default Article
