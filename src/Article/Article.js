abdfffed46ec3279f540118e53671c65e1d6786e
knipferrc <knipferrc@gmail.com>
2017-12-07 07:22:29 -0600

    fix footer

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -7,7 +7,11 @@ const Article = ({ title, author, description, abstract, footer }) => (
     <p className="text-grey mb-3">{author}</p>
     <h2 className="font-sans font-thin leading-normal mb-4">{description}</h2>
     <p className="text-grey-darkest mb-6 leading-tight">{abstract}</p>
-    <div>{footer}</div>
+    <div>
+      {React.cloneElement(footer, {
+        className: 'text-black no-underline'
+      })}
+    </div>
   </div>
 )
 
