9dca031b6b3e797c24098cb43ecc1c905c3afece
knipferrc <knipferrc@gmail.com>
2017-12-12 07:35:51 -0500

    format

--- src/components/ListGroup/ListGroup.js
+++ src/components/ListGroup/ListGroup.js
@@ -3,15 +3,23 @@ import React from 'react'
 
 const ListGroup = ({ list }) => (
   <div className="font-sans">
-  <div className="overflow-hidden bg-white rounded max-w-xs w-full shadow-lg leading-normal">
+    <div className="overflow-hidden bg-white rounded max-w-xs w-full shadow-lg leading-normal">
       {list.map((list, index) => (
-        <a key={index} href="#" className="block group hover:bg-blue p-4 border-b no-underline">
-          <p className="font-bold text-lg mb-1 text-black group-hover:text-white">{list.title}</p>
-          <p className="text-grey-darker mb-2 group-hover:text-white">{list.description}</p>
+        <a
+          key={index}
+          href="#"
+          className="block group hover:bg-blue p-4 border-b no-underline"
+        >
+          <p className="font-bold text-lg mb-1 text-black group-hover:text-white">
+            {list.title}
+          </p>
+          <p className="text-grey-darker mb-2 group-hover:text-white">
+            {list.description}
+          </p>
         </a>
       ))}
+    </div>
   </div>
-</div>
 )
 
 ListGroup.propTypes = {
