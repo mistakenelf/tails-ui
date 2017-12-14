1c6a0e30af70e0adc7f70a5c6c366276dff670c1
knipferrc <knipferrc@gmail.com>
2017-12-14 12:48:28 -0500

    fix navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,18 +4,16 @@ import PropTypes from 'prop-types'
 
 const Navbar = ({ children, brand, color, navRight }) => (
   <div className={`bg-${color} text-white h-24 flex flex-col`}>
-    <div className="flex items-center justify-between">
-      <div className="mt-4 ml-4 flex flex flex-col w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
-        <div className="text-3xl">{brand}</div>
-        <div className="mt-4 overflow-x-scroll">
-          {Children.map(children, child => {
-            return React.cloneElement(child, {
-              className: 'text-white p-1 text-lg'
-            })
-          })}
-        </div>
-      </div>
-      <div className="mr-4">{navRight}</div>
+    <div className="mt-4 pl-4 flex flex flex-row align-center w-full">
+      <div className="text-3xl">{brand}</div>
+      <div className="ml-auto pr-4">{navRight}</div>
+    </div>
+    <div className="mt-4 pl-4 overflow-x-scroll">
+      {Children.map(children, child => {
+        return React.cloneElement(child, {
+          className: 'text-white p-1 text-lg'
+        })
+      })}
     </div>
   </div>
 )
