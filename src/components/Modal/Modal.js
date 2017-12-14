3addb353ec060cc8b04d178ad630a3f0e30a5e0f
knipferrc <knipferrc@gmail.com>
2017-12-14 10:07:58 -0500

    modal component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,29 +1,27 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Modal = ({ title, handleClose, children }) => (
-  <div>
-    <div className="fixed pin flex items-center">
-      <div className="fixed pin bg-black opacity-75 z-10" />
-      <div className="relative mx-6 md:mx-auto w-full md:w-1/2 lg:w-1/3 z-20 m-8">
-        <div className="shadow-lg bg-white p-8">
-          <div className="flex justify-end mb-6">
-            <button onClick={handleClose}>
-              <span className="mr-2">Close</span>
-            </button>
-          </div>
-          <h1 className="text-center text-2xl">{title}</h1>
-          <div>{children}</div>
-        </div>
+const Modal = ({ children, title, handleClose, footer }) => (
+  <div className="fixed pin flex items-center">
+    <div className="fixed pin bg-black opacity-75 z-10" />
+    <div className="p-4 rounded shadow-lg bg-white relative mx-6 md:mx-auto w-full md:w-1/2 lg:w-1/3 z-20 m-8">
+      <div className="flex justify-between items-center mb-6">
+        <div className="text-2xl">{title}</div>
+        <button onClick={handleClose}>
+          <span className="mr-2">close</span>
+        </button>
       </div>
+      <div>{children}</div>
+      <div className="mt-4 flex justify-end">{footer}</div>
     </div>
   </div>
 )
 
 Modal.propTypes = {
+  children: PropTypes.node,
   title: PropTypes.string,
   handleClose: PropTypes.func,
-  children: PropTypes.node
+  footer: PropTypes.node
 }
 
 export default Modal
