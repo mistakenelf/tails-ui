ef8c2ca2ec8aeb463da8d685e3bfd4333cce7457
knipferrc <knipferrc@gmail.com>
2017-12-19 07:54:14 -0500

    cursor pointer

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -14,11 +14,11 @@ const Button = ({
     className={
       outline
         ? `${fullWidth &&
-            'w-full'} bg-transparent hover:bg-${color} text-${color}-dark font-semibold hover:text-white py-2 px-4 border border-${color} hover:border-transparent ${
+            'w-full'} cursor-pointer bg-transparent hover:bg-${color} text-${color}-dark font-semibold hover:text-white py-2 px-4 border border-${color} hover:border-transparent ${
             rounded ? 'rounded-full' : 'rounded'
           } inline-flex items-center justify-center`
         : `${fullWidth &&
-            'w-full'} bg-${color} hover:bg-${color}-dark text-white font-sans font-bold py-2 px-4 shadow ${
+            'w-full'} cursor-pointer bg-${color} hover:bg-${color}-dark text-white font-sans font-bold py-2 px-4 shadow ${
             rounded ? 'rounded-full' : 'rounded'
           } inline-flex items-center justify-center`
     }
